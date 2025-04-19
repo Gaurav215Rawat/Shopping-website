9a92859b60bc3bdd04e5adcdb5e25f63603af4e4
Gaurav Rawat <123564858+Gaurav215Rawat@users.noreply.github.com>
2025-04-19 17:34:46 +0530

    jashba

--- middleware/ratelimit.js
+++ middleware/ratelimit.js
@@ -2,7 +2,8 @@ const rateLimit = require("express-rate-limit");
 
 // Limit OTP requests: max 10 per 15 minutes per IP
 const otpLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs:1 * 60 * 1000, // equals 60000
+  // 15 minutes
   max: 10, // Limit each IP to 10 OTP requests
   message: {
     error: 'Too many OTP requests, please try again after 15 minutes.'
